be5624643bec424420a901dc059b18508a43349c
nonoike <ij4nu8d4fw@gmail.com>
2017-11-10 22:39:20 +0900

    合成関数を作成

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -2,4 +2,12 @@ object Main {
   val f1: Option[Int => Int] = Some((x) => x * 2)
   val f2: Option[Int => Int] = Some((x) => x + 10)
   val f3: Option[Int => Int] = Some((x) => x / 3)
+
+  val compositeFunction: Option[Int => Int] = for {
+    v1 <- f1
+    v2 <- f2
+    v3 <- f3
+  } yield v1 andThen v2 andThen v3
+  compositeFunction.map(_.apply(15))
+
 }
