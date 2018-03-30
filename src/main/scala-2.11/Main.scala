a2a636050ea55668601162e4220c7cfe811f3454
haru221 <gadget.tf@gmail.com>
2018-03-31 01:56:23 +0900

    中級問題の解答

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -2,4 +2,6 @@ object Main {
   val f1: Option[Int => Int] = Some((x) => x * 2)
   val f2: Option[Int => Int] = Some((x) => x + 10)
   val f3: Option[Int => Int] = Some((x) => x / 3)
+
+  val ret = f1.flatMap(i1 => f2.flatMap(i2 => f3.map(i3 => i3(i2(i1(15))))))
 }
