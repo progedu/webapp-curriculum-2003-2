0c194c67f90ab4e2d18b7b04c35085c3e275dbf6
nonoike <12398740+nonoike@users.noreply.github.com>
2018-07-07 00:37:17 +0900

    別解を追加

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -10,4 +10,17 @@ object Main {
   } yield v1 andThen v2 andThen v3
   compositeFunction.map(_.apply(15))
 
+  /*
+  val f1: Option[Int => Int] = Some((x) => x * 2)
+  val f2: Option[Int => Int] = Some((x) => x + 10)
+  val f3: Option[Int => Int] = Some((x) => x / 3)
+
+  def composited(v: Int) = for {
+    a1 <- f1
+    a2 <- f2
+    a3 <- f3
+  } yield a1.andThen(a2).andThen(a3)(v)
+
+  println(composited(15))
+  */
 }
