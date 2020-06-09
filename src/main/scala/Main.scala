435119b973ded224ce15b479ce870fd3067da056
JojiKoike <joji.koike@gmail.com>
2020-06-10 01:08:03 +0900

    不要なカッコの除去、resultの型記述追加

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,9 @@
 object Main {
-  val f1: Option[Int => Int] = Some((x) => x * 2)
-  val f2: Option[Int => Int] = Some((x) => x + 10)
-  val f3: Option[Int => Int] = Some((x) => x / 3)
+  val f1: Option[Int => Int] = Some(x => x * 2)
+  val f2: Option[Int => Int] = Some(x => x + 10)
+  val f3: Option[Int => Int] = Some(x => x / 3)
 
-  val result = for {
+  val result: Option[Int => Int] = for {
     i1 <- f1
     i2 <- f2
     i3 <- f3
