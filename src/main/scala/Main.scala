6240e5e6644b12f7b48f836597a9245242b21bf3
JojiKoike <joji.koike@gmail.com>
2020-06-10 01:06:05 +0900

    for式と関数合成で実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,4 +2,13 @@ object Main {
   val f1: Option[Int => Int] = Some((x) => x * 2)
   val f2: Option[Int => Int] = Some((x) => x + 10)
   val f3: Option[Int => Int] = Some((x) => x / 3)
+
+  val result = for {
+    i1 <- f1
+    i2 <- f2
+    i3 <- f3
+  } yield i1.andThen(i2).andThen(i3)
+
+  result.map(_.apply(15))
+
 }
