e8b70c7fad1223ad39b8583f60a9fe5e1d8e7899
Yuta Takaba <yutakaba@gmail.com>
2020-06-29 01:56:31 +0900

    Output Some(13)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,4 +2,6 @@ object Main {
   val f1: Option[Int => Int] = Some((x) => x * 2)
   val f2: Option[Int => Int] = Some((x) => x + 10)
   val f3: Option[Int => Int] = Some((x) => x / 3)
+  
+  Some(f3.get(f2.get(f1.get(15))))
 }
