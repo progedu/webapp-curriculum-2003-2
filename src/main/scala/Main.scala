636fd8090d6a804f541b78cefcce015713a622a5
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-12 13:28:35 +0900

    calcメソッドの実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
 object Main {
-  val f1: Option[Int => Int] = Some((x) => x * 2)
-  val f2: Option[Int => Int] = Some((x) => x + 10)
-  val f3: Option[Int => Int] = Some((x) => x / 3)
+  val f1: Option[Int => Int] = Some(x => x * 2)
+  val f2: Option[Int => Int] = Some(x => x + 10)
+  val f3: Option[Int => Int] = Some(x => x / 3)
+
+  def calc(x: Int): Option[Int] = 
+    for (i1 <- f1; i2 <- f2; i3 <- f3) yield i3(i2(i1(x)))
 }
